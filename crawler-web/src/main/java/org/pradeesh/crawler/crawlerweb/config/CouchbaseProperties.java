3af8a89d658a4042d3ae73d7dda57ef49da4f751
Pradeesh Kumar <pradeeshbm@ymail.com>
2021-10-12 22:25:10 +0530

    Initial commit

---
+++ crawler-web/src/main/java/org/pradeesh/crawler/crawlerweb/config/CouchbaseProperties.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021. Pradeesh Kumar
+ */
+package org.pradeesh.crawler.crawlerweb.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+/**
+ * The type Couchbase properties.
+ *
+ * @author pradeesh.kumar
+ */
+@ConstructorBinding
+@ConfigurationProperties(prefix = "spring.couchbase")
+public record CouchbaseProperties(String connectionString, String username, String password, String bucketName) {
+}
